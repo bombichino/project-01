5e21ed4d88eac44a5eb44868585b142922564a9d
Viraj Shetty <viraj.vls@gmail.com>
2020-03-21 17:45:58 -0400

    made dynamic version of the website with all working functionalities

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // new york times api key
 
-var nyt_api_key ="YX7AqB8m3C8As3mPU2OpZWGifXMvQ2h1";
-
+var nyt_api_key = "YX7AqB8m3C8As3mPU2OpZWGifXMvQ2h1";
+var omdb_api_key = ""
 
 // actor name splitter function
 
@@ -9,8 +9,9 @@ function actorNameSplitter(actor) {
 
   actorNameSplit = actor.split(" ");
   actorName = "";
+
   for (var i=0; i < actorNameSplit.length; i++) {
-      if (i>0) {
+      if (i>0 & !actorNameSplit ==="") {
       actorName += "+"+ actorNameSplit[i] ;
       }
       else {
@@ -25,6 +26,7 @@ function actorNameSplitter(actor) {
 function articleSearch(actor) {
 
   actorName = actorNameSplitter(actor);
+  // console.log(actorName);
 
   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ actorName + "&api-key=" + nyt_api_key;
 
@@ -33,11 +35,21 @@ function articleSearch(actor) {
       method: "GET"
     }).then(function(response) {
       console.log(response);
+
       $("#nyt-col").empty();
-      $("#nyt-col").append("<br>"); 
+      title = $("<h3>"); 
+      title.text("In News:")
+      $("#nyt-col").append(title); 
+
       for (i=0; i<5;i++) {
         var article = $("<p>");
-        article.text(JSON.parse(JSON.stringify(response.response.docs[i].abstract)));
+        article.text(JSON.parse(JSON.stringify(response.response.docs[i].snippet)));
+        var link = $("<a>");
+        link.attr("href", JSON.parse(JSON.stringify(response.response.docs[i].web_url)));
+        link.attr("target","_blank");
+        link.text(" Read more..");
+        
+        article.append(link);
         $("#nyt-col").append(article);
       }
     });
@@ -57,8 +69,11 @@ function movieReview(actor) {
   }).then(function(response) {
 
     $("#movie-reviews").empty();
-    $("#movie-reviews").append("<br>"); 
-    console.log(response);
+
+    title = $("<h3>"); 
+    title.text("Movie Reviews:")
+    $("#movie-reviews").append(title); 
+
     for (i=0; i<5;i++) {
   
     var review = $("<h6>");
@@ -73,6 +88,7 @@ function movieReview(actor) {
     $("#movie-reviews").append(review);
     $("#movie-reviews").append("<br>"); 
     }
+    $("#movie-reviews").addClass("border-left");
   });
 
 }
@@ -80,91 +96,87 @@ function movieReview(actor) {
 //Pull Actor Names with OMDB API
 
 function moviePull () {
-  var title = $("#movieInput").val();
-  console.log(title);
-  var MovieQuery = "https://www.omdbapi.com/?t=" + title + "&apikey=d58d1281";
-  console.log(MovieQuery);
+  var title = $("#movie-input").val();
+  // console.log(title);
+  var movieQuery = "https://www.omdbapi.com/?t=" + title + "&apikey=d58d1281";
+  // console.log(movieQuery);
   
   $.ajax({
-    url: MovieQuery,
+    url: movieQuery,
     method: "GET"
   }).then(function(response) {
-      console.log(response);
-      $("#movietitle").text(title);
-      var ActorsList = JSON.stringify(response.Actors);
-      ActorsList = ActorsList.replace(/"/g,'');
-      ActorsList = ActorsList.split(",");
-      
-      if (ActorsList[0] != null){
-          $("#actor1").css('visibility', 'visible');
-          $("#actor1").text(ActorsList[0]);
-          ActorButtons2()}
-          else {}
-      
-      function ActorButtons2 () {
-      if (ActorsList[1] != null){
-          $("#actor2").css('visibility', 'visible');
-          $("#actor2").text(ActorsList[1]);
-          ActorButtons3()}
-          else {}}
 
-      function ActorButtons3 () {
-      if (ActorsList[2] != null){
-          $("#actor3").css('visibility', 'visible');
-          $("#actor3").text(ActorsList[2]);
-          ActorButtons4()}
-          else {}}
+    $("#lower-tag").empty();
 
-      function ActorButtons4 () {
-      if (ActorsList[3] != null){
-          $("#actor4").css('visibility', 'visible');
-          $("#actor4").text(ActorsList[3]);
-          ActorButtons5()}
-          else {}}
+      var actorsList = JSON.parse(JSON.stringify(response.Actors));
+      actorsList = actorsList.split(",");
 
-      function ActorButtons5 () {
-      if (ActorsList[4] != null){
-          $("#actor5").css('visibility', 'visible');
-          $("#actor5").text(ActorsList[4]);}
-      else {}
-      };
+      for (var i=0;i< 4;i++) {
+        actor = $('<button>');
+        actor.addClass("actorBtn btn btn-outline-secondary");
+        if (actorsList[i] != null){
+          // $("#actor"+(i+1)).css('visibility', 'visible');
+          actor.text(actorsList[i]);
+          $("#lower-tag").append(actor);
+          $("#lower-tag").addClass("bg-light-yellow rounded");       
+          }
+      }
   });
 };
 
-
 //Giphy pull functions (brings top 5 images to picture1-picture5 divs)
-function giphy(search) {
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=z6IvcDcCGQWaYNeWv3tfp3h19XDJ7V8C";
+function giphy(actor) {
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + actor + "&api_key=z6IvcDcCGQWaYNeWv3tfp3h19XDJ7V8C";
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-  $("#picture1").css("visibility", "visible");
-  $("#picture1").attr("src",response.data[0].images.original.url);
-  $("#picture2").css("visibility", "visible");
-  $("#picture2").attr("src",response.data[1].images.original.url);
-  $("#picture3").css("visibility", "visible");
-  $("#picture3").attr("src",response.data[2].images.original.url);
-  $("#picture4").css("visibility", "visible");
-  $("#picture4").attr("src",response.data[3].images.original.url);
-  $("#picture5").css("visibility", "visible");
-  $("#picture5").attr("src",response.data[4].images.original.url);
+
+    $("#giphy-title").empty();
+    $("#giphy-col").empty();
+    title = $("<h3>"); 
+    title.text("GIFs:");
+    $("#giphy-title").append(title); 
+    for (var i=0;i< 4;i++) {
+        giphyImg = $("<img>");
+        giphyImg.attr("src",JSON.parse(JSON.stringify(response.data[i].images.original.url)));
+        giphyImg.addClass("img-thumbnail");
+        giphyImgCol = $("<div>");
+        giphyImgCol.addClass("col-md-3");
+        giphyImgCol.append(giphyImg);
+        $("#giphy-col").append(giphyImgCol);
+
+        $("#giphy").addClass("border-top border-bottom");
+    };
   });
 }
 
 // event listeners
 
-document.addEventListener('click', function(event) {
-  if (event.target.className.split(" ")[0] === "actorBtn") {
-    var actor = event.target.textContent;
-    articleSearch(actor);
-    movieReview(actor);
-    giphy(event.target.textContent);
-  }
-});
-
+//search button click
 document.getElementById("movieSearchBtn").addEventListener('click', function(event){
   moviePull();
 })
+
+//search bar enter
+document.addEventListener('keypress', function(event) {
+  if (event.target.id === "movie-input" & event.keyCode===13) {
+    moviePull();
+  }
+});
+
+document.addEventListener('click', function(event) {
+  if (event.target.className.split(" ")[0] === "actorBtn") {
+    
+    $("#main-wrapper").removeClass("main");
+    var actor = event.target.textContent;
+    articleSearch(actor);
+    movieReview(actor);
+    giphy(actor);
+  }
+});
+
+
+
 
 
