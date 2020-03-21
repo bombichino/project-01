684db4790a03807bfb957c02c131d17ee88b4333
APPLE BANANA <tedbelanoff@gmail.com>
2020-03-20 23:45:12 -0400

    Added Giphy Scripts/Divs

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ $.ajax({
     }
   });
 
+//Pull Actor Names with OMDB API
   function MoviePull () {
     var title = $(".movieinput").val();
     console.log(title);
@@ -85,3 +86,109 @@ $.ajax({
 $("#movie-search").click(function() {
   MoviePull();
 });
+
+
+//Giphy pull functions (brings top 5 images to picture1-picture5 divs)
+function ButtonPull1() {
+  Search = $("#actor1").text();
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + Search + "&api_key=z6IvcDcCGQWaYNeWv3tfp3h19XDJ7V8C";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+  $("#picture1").css("visibility", "visible");
+  $("#picture1").attr("src",response.data[0].images.original.url);
+  $("#picture2").css("visibility", "visible");
+  $("#picture2").attr("src",response.data[1].images.original.url);
+  $("#picture3").css("visibility", "visible");
+  $("#picture3").attr("src",response.data[2].images.original.url);
+  $("#picture4").css("visibility", "visible");
+  $("#picture4").attr("src",response.data[3].images.original.url);
+  $("#picture5").css("visibility", "visible");
+  $("#picture5").attr("src",response.data[4].images.original.url);
+  });
+}
+
+function ButtonPull2() {
+  Search = $("#actor2").text();
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + Search + "&api_key=z6IvcDcCGQWaYNeWv3tfp3h19XDJ7V8C";
+
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+  }).then(function(response) {
+  $("#picture1").css("visibility", "visible");
+  $("#picture1").attr("src",response.data[0].images.original.url);
+  $("#picture2").css("visibility", "visible");
+  $("#picture2").attr("src",response.data[1].images.original.url);
+  $("#picture3").css("visibility", "visible");
+  $("#picture3").attr("src",response.data[2].images.original.url);
+  $("#picture4").css("visibility", "visible");
+  $("#picture4").attr("src",response.data[3].images.original.url);
+  $("#picture5").css("visibility", "visible");
+  $("#picture5").attr("src",response.data[4].images.original.url);
+  });
+}
+
+function ButtonPull3() {
+  Search = $("#actor3").text()
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + Search + "&api_key=z6IvcDcCGQWaYNeWv3tfp3h19XDJ7V8C";
+  
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+  }).then(function(response) {
+  $("#picture1").css("visibility", "visible");
+  $("#picture1").attr("src",response.data[0].images.original.url);
+  $("#picture2").css("visibility", "visible");
+  $("#picture2").attr("src",response.data[1].images.original.url);
+  $("#picture3").css("visibility", "visible");
+  $("#picture3").attr("src",response.data[2].images.original.url);
+  $("#picture4").css("visibility", "visible");
+  $("#picture4").attr("src",response.data[3].images.original.url);
+  $("#picture5").css("visibility", "visible");
+  $("#picture5").attr("src",response.data[4].images.original.url);
+  });
+}
+
+function ButtonPull4() {
+  Search = $("#actor4").text()
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + Search + "&api_key=z6IvcDcCGQWaYNeWv3tfp3h19XDJ7V8C";
+      
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+  }).then(function(response) {
+  $("#picture1").css("visibility", "visible");
+  $("#picture1").attr("src",response.data[0].images.original.url);
+  $("#picture2").css("visibility", "visible");
+  $("#picture2").attr("src",response.data[1].images.original.url);
+  $("#picture3").css("visibility", "visible");
+  $("#picture3").attr("src",response.data[2].images.original.url);
+  $("#picture4").css("visibility", "visible");
+  $("#picture4").attr("src",response.data[3].images.original.url);
+  $("#picture5").css("visibility", "visible");
+  $("#picture5").attr("src",response.data[4].images.original.url);
+  });
+}    
+
+function ButtonPull5() {
+  Search = $("#actor5").text()
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + Search + "&api_key=z6IvcDcCGQWaYNeWv3tfp3h19XDJ7V8C";    
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+  }).then(function(response) {
+  $("#picture1").css("visibility", "visible");
+  $("#picture1").attr("src",response.data[0].images.original.url);
+  $("#picture2").css("visibility", "visible");
+  $("#picture2").attr("src",response.data[1].images.original.url);
+  $("#picture3").css("visibility", "visible");
+  $("#picture3").attr("src",response.data[2].images.original.url);
+  $("#picture4").css("visibility", "visible");
+  $("#picture4").attr("src",response.data[3].images.original.url);
+  $("#picture5").css("visibility", "visible");
+  $("#picture5").attr("src",response.data[4].images.original.url);
+  });
+}
